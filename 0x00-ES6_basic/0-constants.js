66ad08ecee53eed8253979c0908edd705712dd6f
NavanMapp <navanmapp@gmail.com>
2023-09-26 10:53:53 +0000

    Backend task update

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export const taskFirst = () => {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export let taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
