8241372e3a690ad671bc06c0402e97a2d4c5537a
NavanMapp <navanmapp@gmail.com>
2023-09-26 11:40:56 +0000

    Backend files update

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const reportObject = {
+    allEmployees: employeesList,
+    getNumberOfDepartments: function() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+
+  return reportObject;
+}
