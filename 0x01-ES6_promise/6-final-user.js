6504320fd3666e2e3e7f0c9907665e433a1e1389
Navan Maphalala [IT - Curro St Dominics - All Schools] <adm-navan.m@curro.co.za>
2023-09-28 08:22:18 +0200

    file update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
-
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  });
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [];
+  promises.push(signUpUser(firstName, lastName));
+  promises.push(uploadPhoto(fileName));
+  const results = await Promise.allSettled(promises);
+  const resultArray = results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
+  return resultArray;
 }
+export default handleProfileSignup;
