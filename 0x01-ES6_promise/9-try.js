68fef5d4c0bd220c09890dc1636e1dd84fd735e4
Navan Maphalala [IT - Curro St Dominics - All Schools] <adm-navan.m@curro.co.za>
2023-09-28 08:01:21 +0200

    9-try.js update

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,8 @@
-export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    }
-    return numerator / denominator;
-  } catch (error) {
-    throw error; // Re-throw the caught error
+function divideFunction(dividend, divisor) {
+  if (divisor === 0) {
+    throw new Error("cannot divide by 0");
   }
+  return dividend / divisor;
 }
+
+export default divideFunction;
