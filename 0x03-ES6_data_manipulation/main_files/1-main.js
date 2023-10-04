0d7b90ddc22bd3096db97ea66286297d5b9ee6f2
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2023-10-04 21:40:13 +0200

    Updating files

--- 0x03-ES6_data_manipulation/main_files/1-main.js
+++
@@ -1,5 +0,0 @@
-import getListStudentIds from "../1-get_list_students";
-import getListStudents from "../0-get_list_students.js";
-
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
